803f230377d8b9c85fec02406061af3ea8294f1d
fadilla100 <fadillahusniyahh@gmail.com>
2025-12-15 15:55:50 +0700

    perubahan di routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\GoogleController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('tentang', function () {
+    return view('tentang');
+});
+
+
+Route::get('/sapa/{nama?}', function ($nama = 'semua') {
+    // ↑ '/sapa/{nama}' = URL pattern
+    // ↑ {nama}         = Parameter dinamis, nilainya dari URL
+    // ↑ function($nama) = Parameter diterima di function
+
+    return "Halo, $nama! Selamat datang di Toko Online.";
+    // ↑ "$nama" = Variable interpolation (masukkan nilai $nama ke string)
 });
